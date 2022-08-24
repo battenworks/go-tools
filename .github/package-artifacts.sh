8466504f0c3f2751859747ec5ac2d0bd9362e577
battenworks <david.batten@recursionpharma.com>
2022-08-24 14:01:50 -0600

    build dir stuff

--- .github/package-artifacts.sh
+++ .github/package-artifacts.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eux
 
+ROOT_DIR=PWD
 VERSION=${GITHUB_REF#"refs/tags/"}
 VERSION=${VERSION#"refs/heads/"}
 ARCHIVE_NAME="${BINARY_NAME}_${VERSION}_${GOOS}_${GOARCH}"
@@ -10,8 +11,10 @@ if [ $GOOS == 'windows' ]; then
   EXT='.exe'
 fi
 
+cd ${BUILD_DIR}
 go build -ldflags "-X main.version=${VERSION}" -o ${ARTIFACT_DIR}/${BINARY_NAME}${EXT}
 
+cd ${ROOT_DIR}
 cd ${ARTIFACT_DIR}
 tar cvfz ${ARCHIVE_NAME}.tar.gz "${BINARY_NAME}${EXT}"
 sha256sum ${ARCHIVE_NAME}.tar.gz > ${ARCHIVE_NAME}.sha256
