c4754f6053be6e287377cfe0950dfed52ce5276c
battenworks <david.batten@recursionpharma.com>
2022-08-24 14:11:39 -0600

    use pushd and popd

--- .github/package-artifacts.sh
+++ .github/package-artifacts.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -eux
 
-ROOT_DIR=PWD
 VERSION=${GITHUB_REF#"refs/tags/"}
 VERSION=${VERSION#"refs/heads/"}
 ARCHIVE_NAME="${BINARY_NAME}_${VERSION}_${GOOS}_${GOARCH}"
@@ -11,10 +10,11 @@ if [ $GOOS == 'windows' ]; then
   EXT='.exe'
 fi
 
+pushd
 cd ${BUILD_DIR}
 go build -ldflags "-X main.version=${VERSION}" -o ${ARTIFACT_DIR}/${BINARY_NAME}${EXT}
+popd
 
-cd ${ROOT_DIR}
 cd ${ARTIFACT_DIR}
 tar cvfz ${ARCHIVE_NAME}.tar.gz "${BINARY_NAME}${EXT}"
 sha256sum ${ARCHIVE_NAME}.tar.gz > ${ARCHIVE_NAME}.sha256
