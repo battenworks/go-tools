225b94b15615e5a58c09dc0fb35596579fd48648
battenworks <djb@battenworks.com>
2023-02-26 17:20:57 -0700

    readable version number

--- dc/main.go
+++ dc/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"strings"
 
 	"github.com/battenworks/go-tools/common/v2/console"
 	"github.com/battenworks/go-tools/dc/v2/dccmd"
@@ -10,12 +11,14 @@ import (
 var version string = "built from source"
 
 func main() {
+	readable_version := "Version: " + strings.Replace(version, "dc_v", "", -1)
+
 	if len(os.Args) > 1 {
 		cmd := os.Args[1]
 
 		switch cmd {
 		case "version", "-v", "-version", "--version":
-			console.Outln("version: " + version)
+			console.Outln(readable_version)
 		case "up":
 			result, err := dccmd.Up()
 			if err != nil {
@@ -57,26 +60,26 @@ func main() {
 			console.Out(result)
 			console.Outln("")
 		case "help", "-help", "--help":
-			usage()
+			usage(readable_version)
 		default:
 			result, _ := dccmd.PassThrough(os.Args[1:])
 			console.Out(result)
 		}
 	} else {
-		usage()
+		usage(readable_version)
 	}
 }
 
-func usage() {
+func usage(readable_version string) {
 	console.Whiteln("Wrapper for the Docker Compose CLI")
-	console.Whiteln("Provides some opinionated commands to help with Docker Compose CLI use")
-	console.Whiteln("All other commands are passed directly to the Docker Compose CLI")
+	console.Whiteln("Provides some opinionated commands to help with Docker Compose use")
+	console.Whiteln("All commands not built into this tool are passed directly to Docker Compose")
 	console.Outln("")
-	console.Whiteln("Version: " + version)
+	console.Whiteln(readable_version)
 	console.Outln("")
 	console.Whiteln("Usage: dc COMMAND")
 	console.Outln("")
-	console.Whiteln("commands:")
+	console.Whiteln("Commands:")
 	console.Yellow("up")
 	console.Whiteln("\t- Brings the docker-compose environment up in disconnected mode")
 	console.Yellow("down")
